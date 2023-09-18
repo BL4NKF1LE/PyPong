d5c6fa77b15c8dab969f368663c97258015e3ae6
Juan Francisco Ramírez Vallejo <paco@Juans-Mac-mini.local>
2023-09-17 20:35:44 -0600

    Window color and FPS added.

--- game.py
+++ game.py
@@ -1,16 +1,32 @@
 import pygame
 
-WINDOW_WIDTH, WINDOW_HEIGHT = 700, 500
-WINDOW = pygame.display.set_mode(WINDOW_WIDTH, WINDOW_HEIGHT)
+pygame.init()
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1000, 800
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Paco's Pong")
+FPS = 60
+BACKGROUND_COLOR = (34, 170, 103)
+
+def draw(window):
+    window.fill(BACKGROUND_COLOR)
+    pygame.display.update()
 
 def main():
     running = True
+    clock = pygame.time.Clock()
 
     while running:
+
+        clock.tick(FPS)
+        draw(WINDOW)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = True
+                running = False
                 break
 
-    quit()
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
