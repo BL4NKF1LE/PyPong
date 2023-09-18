8a7f700b09cee71861ebd901e7e97975c942d5cf
Juan Francisco Ramírez Vallejo <paco@Juans-Mac-mini.local>
2023-09-17 22:00:28 -0600

    Ball drawn.

--- game.py
+++ game.py
@@ -9,6 +9,8 @@ FPS = 60
 BACKGROUND_COLOR = (34, 170, 103)
 PADDLE_COLOR = (255, 255, 255)
 PADDLE_WIDTH, PADDLE_HEIGHT = (20, 100)
+BALL_COLOR = (204, 255, 0)
+BALL_RADIUS = 14
 
 class Paddle:
 
@@ -30,14 +32,35 @@ class Paddle:
         
         if not up:
             self.y += self.VELOCITY
-        
-def draw(window, paddles):
+
+class Ball:
+
+    MAX_VELOCITY = 9
+    COLOR = BALL_COLOR
+
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_velocity = self.MAX_VELOCITY
+        self.y_velocity = 0
+
+    def draw(self, window):
+        pygame.draw.circle(window, self.COLOR, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x += self.x_velocity
+        self.y += self.y_velocity
+
+
+def draw(window, paddles, ball):
     window.fill(BACKGROUND_COLOR)
 
     for paddle in paddles:
         paddle.draw(window)
 
     pygame.draw.rect(window, PADDLE_COLOR, (WINDOW_WIDTH//2 - 5, 0, 10, WINDOW_HEIGHT))
+    ball.draw(window)
 
     pygame.display.update()
 
@@ -58,11 +81,12 @@ def main():
 
     player_one_paddle = Paddle(10, WINDOW_HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
     player_two_paddle = Paddle(WINDOW_WIDTH - 10 - PADDLE_WIDTH, WINDOW_HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    ball = Ball(WINDOW_WIDTH//2, WINDOW_HEIGHT//2, BALL_RADIUS)
 
     while running:
 
         clock.tick(FPS)
-        draw(WINDOW, [player_one_paddle, player_two_paddle])
+        draw(WINDOW, [player_one_paddle, player_two_paddle], ball)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -71,6 +95,7 @@ def main():
 
         keys_pressed = pygame.key.get_pressed()
         paddle_movement(keys_pressed, player_one_paddle, player_two_paddle)
+        ball.move()
 
     pygame.quit()
 
